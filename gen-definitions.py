5a032fd2bd0e13503a9a55e1475c6d3d3ba2d65b
Philip Papadopoulos <ppapadop@uci.edu>
2018-11-16 01:01:05 +0000

    Support !include-style semantics

--- gen-definitions.py
+++ gen-definitions.py
@@ -7,6 +7,19 @@ import sys
 import datetime
 import socket 
 import getopt
+import os
+
+class Loader(yaml.SafeLoader):
+	def __init__(self, stream):
+		self._root = os.path.split(stream.name)[0]
+		super(Loader, self).__init__(stream)
+
+	def include(self, node):
+		filename = os.path.join(self._root, self.construct_scalar(node))
+		with open(filename, 'r') as f:
+			return yaml.load(f, Loader)
+
+Loader.add_constructor('!include', Loader.include)
 
 class mkParser(object):
 	def __init__(self):
@@ -18,12 +31,12 @@ class mkParser(object):
 
 	def readPkgYaml(self,fname):
 		with open(fname,"r") as f: 
-			self.kvdict = yaml.load(f)
+			self.kvdict = yaml.load(f,Loader)
 			self.combine()
 	
 	def readDefaultsYaml(self,fname):
 		with open(fname,"r") as f: 
-			self.defaults = yaml.load(f)
+			self.defaults = yaml.load(f,Loader)
 			self.combine()
 
 
@@ -201,7 +214,8 @@ class moduleGenerator(object):
 		self.logger2 = 'if { [ module-info mode load ] } {\n  puts stderr "%s"\n}'
 
 	def gen_header(self):
-		profile = """#%%Module1.0#####################################################################
+		profile = """#%%Module1.0
+#####################################################################
 ## module.skeleton adapted from modulizer script originally written by Harry Mangalam (hjm)
 ## Date: %s
 ## Built on: %s
