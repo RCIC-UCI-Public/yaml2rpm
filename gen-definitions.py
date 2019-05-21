dc4a78ef8885b1ea770ee240b43a4786f9232e3f
Philip Papadopoulos <ppapadop@uci.edu>
2019-05-21 02:37:16 +0000

    Support the YAML2RPM_INC environment variable. Install rpm.extras.yaml and rpm.extras.filter.yaml into the sys directory

--- gen-definitions.py
+++ gen-definitions.py
@@ -12,12 +12,24 @@ import os
 class Loader(yaml.SafeLoader):
 	def __init__(self, stream):
 		self._root = os.path.split(stream.name)[0]
+		self.incPath = ['.']
+		try:
+			iPath = os.environ['YAML2RPM_INC']
+			self.incPath.extend( iPath.split(':'))
+		except:
+			pass	
 		super(Loader, self).__init__(stream)
 
 	def include(self, node):
 		filename = os.path.join(self._root, self.construct_scalar(node))
-		with open(filename, 'r') as f:
-			return yaml.load(f, Loader)
+		# look for filename in the incPath
+		for p in self.incPath:
+			try:
+				with open(os.path.join(p,filename), 'r') as f:
+					return yaml.load(f, Loader)
+			except:
+				pass
+		raise  Exception("%s not found in: %s" % (filename,str(self.incPath)))
 
 Loader.add_constructor('!include', Loader.include)
 
