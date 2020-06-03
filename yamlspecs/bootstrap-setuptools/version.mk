e275d563f5a0ee829b1ccf26a21e157519ad1d6d
Philip Papadopoulos <ppapadop@uci.edu>
2020-06-03 13:01:43 -0700

    Needs to noarch RPM for setuptools. Add provides to setuptools

--- yamlspecs/bootstrap-setuptools/version.mk
+++ yamlspecs/bootstrap-setuptools/version.mk
@@ -2,7 +2,10 @@ NAME 		= python-setuptools
 ARCHIVENAME 	= setuptools
 VERSION 	= 41.0.0
 RELEASE 	= 1
-TARBALL_POSTFIX	= tar.gz
+TARBALL_POSTFIX        = tar.gz
 DISTRO          = $(ARCHIVENAME)-$(VERSION).$(TARBALL_POSTFIX)
 RPM.EXTRAS      = "AutoReq: no"
 RPM.FILES       = /usr/lib/python2.7/site-packages/*\n/usr/bin/*
+RPM.ARCH	= noarch
+RPM.PROVIDES	= python-distribute = $(VERSION)-$(RELEASE) python-setuptools-devel = $(VERSION)-$(RELEASE) python2-setuptools = $(VERSION)-$(RELEASE) 
+
