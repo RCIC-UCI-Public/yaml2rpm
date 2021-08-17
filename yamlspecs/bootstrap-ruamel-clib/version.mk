bbf2a8fb90b72b8e6635189c91a56f71b4e6a595
Nadya Williams <nadya.williams@gmail.com>
2021-08-17 14:08:13 -0700

    need clib for C versions of yaml loader/parser

---
+++ yamlspecs/bootstrap-ruamel-clib/version.mk
@@ -0,0 +1,8 @@
+NAME 		= python-ruamel-yaml-clib
+ARCHIVENAME 	= ruamel.yaml.clib
+VERSION 	= 0.2.2
+RELEASE 	= 1
+TARBALL_POSTFIX	= tar.gz
+DISTRO          = $(ARCHIVENAME)-$(VERSION).$(TARBALL_POSTFIX)
+RPM.EXTRAS      = "AutoReq: no"
+RPM.FILES       = /usr/lib64/python2.7/site-packages
