0d448bd035e11e8199950e2f1c73d835a343eb40
Philip M. Papadopoulos <ppapadop@uci.edu>
2022-01-10 15:31:47 -0800

    Set default ouput level of module command to "concise" (turn off "informational messages")

--- yamlspecs/rcic-modules.sh
+++ yamlspecs/rcic-modules.sh
@@ -4,6 +4,11 @@ if [ ! -v MODULES_AUTO_HANDLING ]; then
    export MODULES_AUTO_HANDLING=1
 fi
 
+# Tell module to command to not be chatty
+if [ ! -v MODULES_VERBOSITY ]; then
+   export MODULES_VERBOSITY=concise
+fi
+
 if [ -f /opt/rcic/Modules/init/bash ]; then
   . /opt/rcic/Modules/init/bash
 fi
