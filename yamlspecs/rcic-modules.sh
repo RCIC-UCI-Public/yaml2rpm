cf2cc5d53c366a576045f961ea982a7996f619c9
Philip M. Papadopoulos <ppapadop@uci.edu>
2022-01-10 15:19:27 -0800

    RHEL8 version of modules now does autoloading/unloading for prereq modules, so use it. 1. modify rpm.yaml to (quietly) load/unload modules when doing various packaging checks. 2. modify builder/Makefile to give --auto to the module load command, but only on RHEL8 3. modify rcic-modules.sh to set the appropriate shell variable to make this happen

--- yamlspecs/rcic-modules.sh
+++ yamlspecs/rcic-modules.sh
@@ -1,4 +1,9 @@
 
+# on RHEL8 load/unload prereq modules automatically
+if [ ! -v MODULES_AUTO_HANDLING ]; then
+   export MODULES_AUTO_HANDLING=1
+fi
+
 if [ -f /opt/rcic/Modules/init/bash ]; then
   . /opt/rcic/Modules/init/bash
 fi
