5ceaaa2043d75f6d833ea00ec5d7eb00983c1b32
Philip Papadopoulos <ppapadop@uci.edu>
2023-03-20 14:04:41 -0700

    Make bin-python essentially vacuous on Redhat 7.  It should only update alternatives and claim it provides /bin/python in EL8

--- yamlspecs/bootstrap-bin-python/version.mk
+++ yamlspecs/bootstrap-bin-python/version.mk
@@ -1,10 +1,14 @@
 NAME 		= bin-python
 VERSION 	= 3
-RELEASE 	= 1
-RPM.DESCRIPTION     = Use alternatives to set /usr/bin/python to /usr/bin/python3. Also provides /usr/bin/python for RPMS that require unversioned python. It will not change an existing /usr/bin/python file.
+RELEASE 	= 2
+RPM.DESCRIPTION     = Use alternatives to set /usr/bin/python to /usr/bin/python3. Also provides /usr/bin/python for RPMS that require unversioned python. It will not change an existing /usr/bin/python file. It will do NONE of the above if installed on Redhat/CentOS 7
 RPM.ARCH        = noarch
-RPM.REQUIRES    = /usr/sbin/alternatives /usr/bin/python3
+ifneq ($(strip $(ROCKS.OS.VERSION.MAJOR)), 7)
 RPM.PROVIDES    = /usr/bin/python
+RPM.REQUIRES    = /usr/sbin/alternatives /usr/bin/python3 /etc/os-release
+else
+RPM.REQUIRES    = /etc/os-release
+endif
 RPM.SCRIPTLETS.FILE = scriptlets
 RPM.ROOT	= /usr/share/$(NAME)
 RPM.FILES	= $(RPM.ROOT)
